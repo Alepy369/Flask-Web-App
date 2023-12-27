2c8d68875035a6f7c1c42a61709b72f302cc1151
Alepy369 <duartea369@gmail.com>
2023-12-27 21:38:49 +0000

    website updated

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, session, request
 from os import path
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager, logout_user, current_user
+from datetime import datetime, timedelta, timezone
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -10,6 +11,7 @@ def create_app():
     app = Flask(__name__)
     app.secret_key = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    
     db.init_app(app)
 
     from .views import views
@@ -30,6 +32,23 @@ def create_app():
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
+    
+    @app.before_first_request
+    def before_request():
+        session.permanent = True
+        app.permanent_session_lifetime() = timedelta(seconds=15)
+        if current_user.is_authenticated:
+            last_active = session.get('last_active')
+            utc_now = datetime.now(timezone.utc)  # Current aware UTC time
+            if last_active:
+                duration = (utc_now - last_active).total_seconds()
+                print(f"Time since last activity: {duration} seconds")
+                if duration > app.permanent_session_lifetime():
+                    print("Session expired. Logging out user.")
+                    logout_user()
+
+            # Update the last activity time in the session for the current user
+            session['last_active'] = utc_now
 
     return app
 
